2a2cfa8e9cd40da5eeda782f80dbb17b3e7b6069
Mike Webster <email@mikewebster.tech>
2021-02-14 20:55:24 -0600

    WIP + separating js and css further

--- main.js
+++ main.js
@@ -1,65 +1,41 @@
-window.onload = function(){
-    var menuIconOpen = document.getElementsByClassName("open")[0];
-    var menuIconClose = document.getElementsByClassName("close")[0];
-    var menuItems = document.getElementsByClassName("menu");
-    
-    menuIconClose.style.display = "none";
-    
-    menuIconOpen.onclick = function() {
-        this.style.display = "none";
-        menuIconClose.style.display = "block";
-        menuDisplay("show", menuItems);
-    };
-    
-    menuIconClose.onclick = function() {
-        this.style.display = "none";
-        menuIconOpen.style.display = "block";
-        menuDisplay("hide", menuItems);
-    };
+window.addEventListener("load", setupContactButton);
 
+function setupContactButton() {
     var btn = document.getElementById("submit");
     btn.addEventListener("click", function(){
-        clearValidation();
-
-        var title = document.getElementById("title").value;
-        var msg = document.getElementById("message").value;
-
-        var errors = [];
-
-        if (title.trim().length < 1) {
-            errors.push("Please include a title!");
-        }
-
-        if (msg.trim().length < 1) {
-            errors.push("Please include a message!");
-        }
-
-        if (errors.length > 0) {
-            var container = document.getElementById("validationContainer");
-            for (var i = 0; i < errors.length; i++) {
-                container.innerHTML += "<div class='error text-center'>" + errors[i] + "</div>";
-            }
-            container.classList.remove("hide");
-            return
-        }
-        window.open('mailto:email@mikewebster.tech?subject=' + title + '&body=' + msg);
+        contactClick();
     });
-};
+}
+
+function contactClick() {
+    clearValidation();
+
+    var title = document.getElementById("title").value;
+    var msg = document.getElementById("message").value;
+
+    var errors = [];
+
+    if (title.trim().length < 1) {
+        errors.push("Please include a title!");
+    }
+
+    if (msg.trim().length < 1) {
+        errors.push("Please include a message!");
+    }
+
+    if (errors.length > 0) {
+        var container = document.getElementById("validationContainer");
+        for (var i = 0; i < errors.length; i++) {
+            container.innerHTML += "<div class='error text-center'>" + errors[i] + "</div>";
+        }
+        container.classList.remove("hide");
+        return
+    }
+    window.open('mailto:email@mikewebster.tech?subject=' + title + '&body=' + msg);
+}
 
 function clearValidation() {
     var container = document.getElementById("validationContainer");
     container.innerHTML = "";
     container.classList.add("hide");
-}
-
-function menuDisplay(state, items){
-    if(state=="show"){
-        for(var i=1; i<items.length; i++){
-        items[i].classList.add("show");
-        }
-    } else {
-        for(var i=1; i<items.length; i++){
-        items[i].classList.remove("show");
-        }
-    }
 }
